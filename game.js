5580671f4739e899eb4d15d0d6f95cdc62b6163d
helloitserin <erintcl2023@student.cis.edu.hk>
2017-04-12 15:25:14 +0800

    Rock Paper Scissors works

--- game.js
+++ game.js
@@ -1,7 +1,5 @@
 $(document).on("pagecreate","#firstgame",function(event){
   
-
-
 var score=0;
 var pic1 = document.getElementById("rock");
 var pic2= document.getElementById("paper");
@@ -10,22 +8,80 @@ var reset= document.getElementById("buttonone");
 var test= document.getElementById ("test");
 var clickapic= document.getElementById("clickapic");
 
+//RANDOMIZES computer choice
+function getCompChoice(){
+	var choices=['rock','paper','scissors']
+	var compChoice=choices[Math.floor(Math.random()*choices.length)];
+	return compChoice;
+}
+
 pic1.addEventListener('click', function(){
+  var computerChoice = getCompChoice();
+  
+  if (computerChoice === "scissors") {
   score=score+1;
   test.innerHTML=score;
-  clickapic.innerHTML = "You picked rock. Computer picked scissors. You win!";
+  clickapic.innerHTML = "You picked rock. Computer picked Scissors. You win!";
+}
+
+else if (computerChoice === "paper") {
+score=score-1;
+test.innerHTML=score;
+  clickapic.innerHTML = "You picked rock. Computer picked Paper. You lose!";
+}
+
+else {
+	score=score+0;
+	test.innerHTML=score;
+	clickapic.innerHTML = "It's a tie!";
+}
+
 });
 
 pic2.addEventListener('click', function(){
-  score=score-1;
-  test.innerHTML=score;
-  clickapic.innerHTML = "You picked scissors. Computer picked rock. You lose!";
+	var computerChoice = getCompChoice();
+
+	if (computerChoice === "scissors"){
+		score=score-1;
+		test.innerHTML=score;
+		clickapic.innerHTML = "You picked paper. Computer picked scissors. You lose!";
+	}
+
+else if (computerChoice ==="rock"){
+		score=score+1;
+		test.innerHTML=score;
+		clickapic.innerHTML = "You picked paper. Computer picked rock. You win!";
+}
+
+else{
+	score=score+0;
+	test.innerHTML=score;
+	clickapic.innerHTML = "it's a tie!";
+}
+
 });
 
 pic3.addEventListener('click',function(){
-	score=score+0;
-	test.innerHTML=score
-	clickapic.innerHTML = "You and the Computer both picked paper. It's a tie."
+	var computerChoice = getCompChoice();
+
+	if (computerChoice === "paper") {
+		score=score+1;
+		test.innerHTML=score;
+		clickapic.innerHTML = "You picked scissors. Computer picked paper. You win!";
+	}
+
+	else if (computerChoice === "rock") {
+		score=score-1;
+		test.innerHTML=score;
+		clickapic.innerHTML = "You picked scissors. Computer picked rock. You lose!";
+	}
+
+	else{
+		score=score+0;
+		test.innerHTML=score;
+		clickapic.innerHTML = "It's a tie.";
+	}
+	
 });
 
 reset.addEventListener('click', function(){
