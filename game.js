88b5dc22560dada2643db46629987c1e8ca0e700
helloitserin <erintcl2023@student.cis.edu.hk>
2017-04-13 10:07:58 +0800

    Comp Score

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 $(document).on("pagecreate","#firstgame",function(event){
   
 var score=0;
+var compScore=0;
 var pic1 = document.getElementById("rock");
 var pic2= document.getElementById("paper");
 var pic3= document.getElementById("scissors");
 var reset= document.getElementById("buttonone");
 var test= document.getElementById ("test");
+var compPoints = document.getElementById('compscor');
 var clickapic= document.getElementById("clickapic");
 
 //RANDOMIZES computer choice
@@ -20,18 +22,24 @@ $('#rock').on('tap', function(){
   
   if (computerChoice === "scissors") {
   score=score+1;
+  compScore=compScore-1;
+  compPoints.innerHTML=compScore;
   test.innerHTML=score;
   clickapic.innerHTML = "You picked rock. Computer picked Scissors. You win!";
 }
 
 else if (computerChoice === "paper") {
 score=score-1;
+compScore=compScore+1;
+compPoints.innerHTML=compScore;
 test.innerHTML=score;
   clickapic.innerHTML = "You picked rock. Computer picked Paper. You lose!";
 }
 
 else {
 	score=score+0;
+	compScore=compScore+0;
+	compPoints.innerHTML=compScore;
 	test.innerHTML=score;
 	clickapic.innerHTML = "It's a tie!";
 }
@@ -43,18 +51,24 @@ $('#paper').on('tap', function(){
 
 	if (computerChoice === "scissors"){
 		score=score-1;
+		compScore=compScore+1;
+		compPoints.innerHTML=compScore;
 		test.innerHTML=score;
 		clickapic.innerHTML = "You picked paper. Computer picked scissors. You lose!";
 	}
 
 else if (computerChoice ==="rock"){
 		score=score+1;
+		compScore=compScore-1;
+  		compPoints.innerHTML=compScore;
 		test.innerHTML=score;
 		clickapic.innerHTML = "You picked paper. Computer picked rock. You win!";
 }
 
 else{
 	score=score+0;
+	compScore=compScore+0;
+	compPoints.innerHTML=compScore;
 	test.innerHTML=score;
 	clickapic.innerHTML = "it's a tie!";
 }
@@ -66,18 +80,24 @@ $('#scissors').on('tap', function(){
 
 	if (computerChoice === "paper") {
 		score=score+1;
+		compScore=compScore-1;
+  		compPoints.innerHTML=compScore;
 		test.innerHTML=score;
 		clickapic.innerHTML = "You picked scissors. Computer picked paper. You win!";
 	}
 
 	else if (computerChoice === "rock") {
 		score=score-1;
+		compScore=compScore+1;
+		compPoints.innerHTML=compScore;
 		test.innerHTML=score;
 		clickapic.innerHTML = "You picked scissors. Computer picked rock. You lose!";
 	}
 
 	else{
 		score=score+0;
+		compScore=compScore+0;
+		compPoints.innerHTML=compScore;
 		test.innerHTML=score;
 		clickapic.innerHTML = "It's a tie.";
 	}
@@ -85,7 +105,9 @@ $('#scissors').on('tap', function(){
 });
 
 reset.addEventListener('click', function(){
-  score=score=0;
+  score=0;
+  compScore=0;
+  compPoints.innerHTML=compScore;
   test.innerHTML=score; 
   clickapic.innerHTML = "Click a picture."
 });
